b15bf9d085ad0a16220ae80d9c7d7eeb222b90b7
guptavishu1000 <guptavishu1000@gmail.com>
2025-08-27 01:54:09 +0530

    minor change

--- backend/build.sh
+++ backend/build.sh
@@ -7,5 +7,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+echo "Listing Django users..."
+python manage.py shell -c "from django.contrib.auth.models import User; print(list(User.objects.values('username','is_staff','is_superuser')))"
+
 # Use our new, safe command that won't fail
 python manage.py createsuperuser --noinput || true
