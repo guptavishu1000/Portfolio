5a105623be1fdd6938b4478958b4e00ad4b40081
guptavishu1000 <guptavishu1000@gmail.com>
2025-08-26 23:09:01 +0530

    changes build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
+# exit on error
 set -e
 set -u
 set -o pipefail
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Run build commands
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# (Optional) Create a superuser if the environment variable is set
+if [[ -n "${CREATE_SUPERUSER-}" ]]; then
+  echo "Creating superuser..."
+  python manage.py createsuperuser --no-input
+fi
