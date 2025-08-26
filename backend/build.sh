7b99e560d59c50e80ded0bd6a7431102a93cb6f6
guptavishu1000 <guptavishu1000@gmail.com>
2025-08-27 02:19:54 +0530

    minor change

--- backend/build.sh
+++ backend/build.sh
@@ -11,4 +11,4 @@ echo "Listing Django users..."
 python manage.py shell -c "from django.contrib.auth.models import User; print(list(User.objects.values('username','is_staff','is_superuser')))"
 
 # Use our new, safe command that won't fail
-python manage.py setup_portfolio --force
+python manage.py createsuperuser --noinput || true
