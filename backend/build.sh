62f01a8d02af0a06c24f3a388d65987321d62274
guptavishu1000 <guptavishu1000@gmail.com>
2025-09-18 12:20:46 +0530

    minor fix

--- backend/build.sh
+++ backend/build.sh
@@ -5,7 +5,19 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+
+# Show existing migrations
+echo "Showing existing migrations:"
+python manage.py showmigrations
+
+# Apply migrations with more verbosity
+echo "Applying migrations..."
+python manage.py migrate --no-input
+
+# Show migrations after applying
+echo "Migrations after applying:"
+python manage.py showmigrations
 
 echo "Listing Django users..."
 python manage.py shell -c "from django.contrib.auth.models import User; print(list(User.objects.values('username','is_staff','is_superuser')))"
