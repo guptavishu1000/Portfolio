69c9be046bc6935f60d2f6a42e5fa19e43e4cc8f
guptavishu1000 <guptavishu1000@gmail.com>
2025-09-18 13:03:24 +0530

    minor check

--- backend/build.sh
+++ backend/build.sh
@@ -3,17 +3,19 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 
 
 # Show existing migrations
-echo "Showing existing migrations:"
-python manage.py showmigrations
+echo "Checking applied migrations..."
+python manage.py showmigrations portfolio_api
+
+echo "SQL for 0001_initial (just for reference):"
+python manage.py sqlmigrate portfolio_api 0001_initial
 
 # Apply migrations with more verbosity
 echo "Applying migrations..."
-python manage.py migrate --no-input --fake-initial
+python manage.py migrate --no-input
 
 # Show migrations after applying
 echo "Migrations after applying:"
